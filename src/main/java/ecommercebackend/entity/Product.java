3d837ab6819f28376573ea2f68b20f8fc74f7ad2
Haripriya <haripriyatripathi@users.noreply.github.com>
2026-06-23 02:54:22 +0530

    add product entity

---
+++ src/main/java/ecommercebackend/entity/Product.java
@@ -0,0 +1,30 @@
+package ecommercebackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter
+@Setter
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(length = 500)
+    private String description;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer stockQuantity;
+
+    private String imageUrl;
+}
